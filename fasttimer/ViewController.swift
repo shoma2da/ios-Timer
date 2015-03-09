a39bec8bebb44f552050693b9b68d4998d5ad305
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-03-09 12:56:22 +0900

    Initial Commit

---
+++ fasttimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fasttimer
+//
+//  Created by Shoichi Matsuda on 2015/03/09.
+//  Copyright (c) 2015年 indivisual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
